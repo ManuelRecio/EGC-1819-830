8f51bbe21d6b615e20130de1fee93be62f582239
ManuelRecio <man.recio.real@gmail.com>
2021-02-10 20:59:20 +0100

    mensaje editado

---
+++ desarrollo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/manu/Escritorio/repo_examen_prueba/EGC-1819-830/desarrollo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
